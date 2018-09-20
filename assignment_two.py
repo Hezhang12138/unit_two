8376c58b436fb191dc88463fd0ed777750be9b6d
Frank <frank@iMac3.local>
2018-09-20 10:16:06 -0400

    I learn how to do a chat box today

---
+++ assignment_two.py
@@ -0,0 +1,33 @@
+What_is_your_name = input("What is your name?:")
+
+print("You did a great job", What_is_your_name, "and another question.")
+
+where_are_you_come_from = input("Where are you come from?")
+
+print(where_are_you_come_from, "is a beautiful place, isn't it?")
+
+What_is_your_favorite_number = input("What is your favorite number?")
+
+my_favorite_number = 6
+
+times = int(What_is_your_favorite_number) / int(my_favorite_number)
+
+print(What_is_your_favorite_number, "is a great number!", "which is", times, "times larger than my favorite number 6")
+
+What_is_your_dream_car = input("What is your dream car?:")
+
+print("Emmm, I love", What_is_your_dream_car, "do you know more about it?")
+
+How_much_does_your_dream_car_cost = input("How much does your dream car cost?:")
+
+print("Wow,", How_much_does_your_dream_car_cost, "is a lot.")
+
+How_many_years_the_user_would_take_a_loan_out_to_pay = input("how many years would you take a loan out to pay")
+
+print(How_many_years_the_user_would_take_a_loan_out_to_pay, "is a long time!")
+
+What_annual_interest_rate_you_expect_to_get = input("what annual interest rate are you expect to get?")
+
+print(What_annual_interest_rate_you_expect_to_get, "Emmm, I believe you can do it!")
+
+print("I think we have a great talk! Bye")
